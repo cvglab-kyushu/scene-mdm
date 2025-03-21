0736d90bf800c0665de6c89308ea5e12a93a0a12
teshit <teshima.058@gmail.com>
2025-03-21 21:08:12 +0900

    fixed

--- scripts/generate_final_motion.sh
+++ scripts/generate_final_motion.sh
@@ -4,7 +4,7 @@ python finetuned_motion_control_posa_trajectory.py \
     --model_path "save/humanml_root_key_y/model.pt" \
     --dataset "humanml_266" \
     --guidance_param 1.0 \
-    --num_samples 20 \
+    --num_samples 3 \
     --show_input \
     --inpainting_mask "root_key_y" \
     --posa_output_path "./save/humanml_only_text_condition/result_the_person_walks_and_sits_on_a_chair/sample00_rep00_iter=20_affordance/pkl/MPH11/075.npy" \
